7a06eff1dbf0c2165cbc7248076636d7cd18ed91
Allison Nguyen <afnguyen@purdue.edu>
2025-09-11 16:10:15 -0400

    WIP: added quit button functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,4 +12,15 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Scores");
     }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
 }
