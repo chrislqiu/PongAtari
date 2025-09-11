84895e1754fa06b81835d911c71a64039731f717
unknown <chris.qiu52@gmail.com>
2025-09-11 16:28:59 -0400

    added blur (not to text), added space between  ><

--- Assets/Scripts/MenuOptionHover.cs
+++ Assets/Scripts/MenuOptionHover.cs
@@ -7,31 +7,19 @@ public class MenuOptionHover : MonoBehaviour, IPointerEnterHandler, IPointerExit
     private TextMeshProUGUI textMesh;
     private string originalText;
 
-    private Color normalColor = new Color(0.5f, 0.5f, 0.5f);
-    private Color hoverColor = Color.white;
-
     void Start()
     {
         textMesh = GetComponentInChildren<TextMeshProUGUI>();
         originalText = textMesh.text;
-
-        // Force material creation (this is the key part)
-        textMesh.fontMaterial = new Material(textMesh.fontMaterial);
-        textMesh.color = normalColor;
-        textMesh.ForceMeshUpdate();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        textMesh.text = ">" + originalText + "<";
-        textMesh.color = hoverColor;
-        textMesh.ForceMeshUpdate();
+        textMesh.text = "> " + originalText + " <";
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         textMesh.text = originalText;
-        textMesh.color = normalColor;
-        textMesh.ForceMeshUpdate();
     }
 }
