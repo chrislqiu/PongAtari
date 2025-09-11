0d986fd34dfb4172b2b47e23eb0e0fa9599ca303
Allison Nguyen <afnguyen@purdue.edu>
2025-09-11 17:07:22 -0400

    WIP: changed stylization and title of selected buttons

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,55 @@
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Canvas targetCanvas;
+    private Button[] menuButtons;
+
+    void Start()
+    {
+        if (targetCanvas != null)
+        {
+            menuButtons = targetCanvas.GetComponentsInChildren<Button>();
+
+            foreach (Button button in menuButtons)
+            {
+                button.onClick.AddListener(() => OnButtonClick(button));
+            }
+        }
+        else
+        {
+            Debug.LogError("Target Canvas is not assigned. Please assign it in the Inspector.");
+        }
+    }
+
+    void OnButtonClick(Button button)
+    {
+        // Reset all button titles
+        foreach (Button btn in menuButtons)
+        {
+            TextMeshProUGUI text = btn.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = StripArrows(text.text);
+        }
+
+        // Change title of selected button
+        TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = AddArrows(buttonText.text);
+    }
+
+    string AddArrows(string title)
+    {
+        return $"> {StripArrows(title)} <";
+    }
+
+    string StripArrows(string title)
+    {
+        return Regex.Replace(title, @"^\s*>\s*(.*?)\s*<\s*$", "$1");
+    }
+
     public void NewGame()
     {
         SceneManager.LoadScene("Game");
@@ -12,7 +59,7 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Scores");
     }
-    
+
     public void Credits()
     {
         SceneManager.LoadScene("Credits");
