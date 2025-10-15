9d721dd0d97665cc068cbbf1d1d2d3b4c01bbad3
Allison Nguyen <afnguyen@purdue.edu>
2025-10-15 12:16:20 -0400

    WIP: Consolidated menu management files

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,17 +1,93 @@
+using System.Collections;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public Canvas targetCanvas;
+    public SceneChanger sceneChanger;
+    [SerializeField] float sceneDelay = 0.5f;
+    private Button[] menuButtons;
+
+    void Start()
+    {
+        if (targetCanvas != null)
+        {
+            menuButtons = targetCanvas.GetComponentsInChildren<Button>();
+
+            foreach (Button button in menuButtons)
+            {
+                button.onClick.AddListener(() => OnButtonClick(button));
+            }
+        }
+        else
+        {
+            Debug.LogError("Target Canvas is not assigned. Please assign it in the Inspector.");
+        }
+    }
+
+    /* ------------------------------------- Button Behavior ------------------------------------- */
+
+    void OnButtonClick(Button button)
+    {
+        // Reset all button titles
+        foreach (Button btn in menuButtons)
+        {
+            TextMeshProUGUI text = btn.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = StripArrows(text.text);
+        }
+
+        // Change title of selected button
+        TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = AddArrows(buttonText.text);
+    }
+
+    string AddArrows(string title)
+    {
+        return $"> {StripArrows(title)} <";
+    }
+
+    string StripArrows(string title)
+    {
+        return Regex.Replace(title, @"^\s*>\s*(.*?)\s*<\s*$", "$1");
+    }
+
+    /* -------------------------------------- Scene Changes -------------------------------------- */
+
+    private IEnumerator LoadSceneAfterDelay(string sceneName)
+    {
+        yield return new WaitForSeconds(sceneDelay);
+        sceneChanger.FadeToLevel(sceneName);
+    }
+
+    public void LoadGameOptions() => StartCoroutine(LoadSceneAfterDelay("SingleMulti"));
+
     public void StartSinglePlayer()
     {
         GameManager.Instance.isAI = true; // right paddle = AI
-        SceneManager.LoadScene("GameStart"); // load game scene
+        StartCoroutine(LoadSceneAfterDelay("GameStart"));
     }
 
     public void StartMultiplayer()
     {
         GameManager.Instance.isAI = false; // right paddle = human
-        SceneManager.LoadScene("GameStart"); // load game scene
+        StartCoroutine(LoadSceneAfterDelay("GameStart"));
+    }
+
+    public void HighScores() => StartCoroutine(LoadSceneAfterDelay("Scores"));
+
+    public void Credits() => StartCoroutine(LoadSceneAfterDelay("Credits"));
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
